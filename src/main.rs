9d8e27da98b84906d20b3d99aef52df98b993880
max <me@mclifford.dev>
2024-07-05 19:10:38 +0100

    Work on main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use crate::server::{ServiceBusServer, ServiceBusServerConfiguration};
+
 mod routes;
+mod store;
+mod server;
 
 fn main() {
-    println!("Hello, world!");
+
+    let cfg =
+        ServiceBusServerConfiguration::default()
+            .with_address("127.0.0.1:1469".to_string())
+            .with_store_path("".to_string())
+            .with_console_sink();
+
+    ServiceBusServer::start(cfg);
 }
