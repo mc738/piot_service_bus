62e6df2b2e9697b72cab43256291b895817cc821
max <me@mclifford.dev>
2024-07-25 20:02:12 +0100

    Work on utils.rs

--- src/store/v1/utils.rs
+++ src/store/v1/utils.rs
@@ -1,5 +1,12 @@
 use serde_json::{Map, Value};
 
-pub fn get_json_string_value(obj: Map<String, Value>) {
-    obj.get()
+pub fn get_json_string_value(obj: Map<String, Value>, key: String) -> Option<String> {
+    obj.get(&key).and_then(|v| {
+        match v {
+            Value::String(s) => {
+                Some(s.clone())
+            }
+            _ => None
+        }
+    })
 }
